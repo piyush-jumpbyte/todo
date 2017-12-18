fe91748cb6bd9faabcf2f81c86e25c2ff4d3dd75
piyush koradiya <piyush@jumpbyte.com>
2017-12-18 18:39:37 +0530

    remove

--- temp/temp.js
+++
@@ -1,9 +0,0 @@
-schema.todoModel.find({_id:temporaryid},(error,todoModels)=>{
-            schema.commentModel.find({todoId:temporaryid},(error,commentModels)=>{
-                console.log('commentModels');
-                res.render('display',{
-                    todo: todoModels[0],
-                    comments: commentModels
-                })
-            });
-        });   
