10acf78ada0bbb2f45ce254fda1190d7cdbfee52
piyush koradiya <piyush@jumpbyte.com>
2017-12-18 18:37:01 +0530

    remove unnecessary folders

--- commit/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-const server = http.createServer(app);
-
-server.listen(8080, function(req,res){
-  console.log('server started : 8080')
-})
